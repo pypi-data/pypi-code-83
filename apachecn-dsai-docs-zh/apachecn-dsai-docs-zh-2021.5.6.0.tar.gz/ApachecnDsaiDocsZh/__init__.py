5484953a4c2137d3314ff5ef2b8be657eedd74d1
Tom Forbes <tom@tomforb.es>
2021-05-06 14:20:53 +0000

    {"name":"apachecn-dsai-docs-zh","version":"2021.5.6.0","file":"apachecn-dsai-docs-zh-2021.5.6.0.tar.gz","path":"apachecn-dsai-docs-zh/apachecn-dsai-docs-zh-2021.5.6.0.tar.gz"}

---
+++ apachecn-dsai-docs-zh/apachecn-dsai-docs-zh-2021.5.6.0.tar.gz/ApachecnDsaiDocsZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-dsai-docs-zh
+https://github.com/apachecn/apachecn-dsai-docs-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.5.6.0"
