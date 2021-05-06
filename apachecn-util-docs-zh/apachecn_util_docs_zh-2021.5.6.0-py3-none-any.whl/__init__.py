d9197a029a1712e76b0405c66c25dcc2848dc834
Tom Forbes <tom@tomforb.es>
2021-05-06 12:46:59 +0000

    {"name":"apachecn-util-docs-zh","version":"2021.5.6.0","file":"apachecn_util_docs_zh-2021.5.6.0-py3-none-any.whl","path":"apachecn-util-docs-zh/apachecn_util_docs_zh-2021.5.6.0-py3-none-any.whl"}

---
+++ apachecn-util-docs-zh/apachecn_util_docs_zh-2021.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-util-docs-zh
+https://github.com/apachecn/apachecn-util-docs-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.5.6.0"
