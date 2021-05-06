cd4ad1e1b6e00fca4ea3f826e0a7ae3e73e29603
Tom Forbes <tom@tomforb.es>
2021-05-06 13:12:44 +0000

    {"name":"apachecn-python-docs-zh","version":"2021.5.6.0","file":"apachecn-python-docs-zh-2021.5.6.0.tar.gz","path":"apachecn-python-docs-zh/apachecn-python-docs-zh-2021.5.6.0.tar.gz"}

---
+++ apachecn-python-docs-zh/apachecn-python-docs-zh-2021.5.6.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ApachecnPythonDocsZh
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ApachecnPythonDocsZh'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="apachecn-python-docs-zh",
+    version=ApachecnPythonDocsZh.__version__,
+    url="https://github.com/apachecn/apachecn-python-docs-zh",
+    author=ApachecnPythonDocsZh.__author__,
+    author_email=ApachecnPythonDocsZh.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="ApacheCN Python 中文文档汇总",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "apachecn-python-docs-zh=ApachecnPythonDocsZh.__main__:main",
+            "ApachecnPythonDocsZh=ApachecnPythonDocsZh.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
